0ed19591e9bf00367be594698f4808423ee207d1
patryk.bujalla <patryk.bujalla@portacapena.com>
2020-12-08 14:07:11 +0100

    default odoo test base data

--- PortaCapena.OdooJsonRpcClient.Shared/OdooTestBase.cs
+++ PortaCapena.OdooJsonRpcClient.Shared/OdooTestBase.cs
@@ -5,16 +5,10 @@ namespace PortaCapena.OdooJsonRpcClient.Shared
     public abstract class OdooTestBase
     {
         protected static readonly OdooConfig Config = new OdooConfig(
-            //apiUrl: "https://lipa-fenix-staging1-1757500.dev.odoo.com",
-            //dbName: "lipa-fenix-staging1-1757500",
-            //userName: "admin",
-            //password: "admin"
-
-
-        apiUrl: "https://thegreencommunity-test-staging2-1793197.dev.odoo.com",
-        dbName: "thegreencommunity-test-staging2-1793197",
-        userName: "admin",
-        password: "99400891PC"
+            apiUrl: "https://odoo-api-url.com",
+            dbName: "odoo-db-name",
+            userName: "admin",
+            password: "admin"
         );
     }
 }
