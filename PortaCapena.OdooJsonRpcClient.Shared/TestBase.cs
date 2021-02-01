88fc7f62ccbb8fcae415ab4571e039eec068284b
patryk.bujalla <patryk.bujalla@portacapena.com>
2021-02-01 15:20:24 +0100

    clear

--- PortaCapena.OdooJsonRpcClient.Shared/TestBase.cs
+++ PortaCapena.OdooJsonRpcClient.Shared/TestBase.cs
@@ -5,8 +5,8 @@ namespace PortaCapena.OdooJsonRpcClient.Shared
     public class TestBase
     {
         protected static readonly OdooConfig Config = new OdooConfig(
-            apiUrl: "https://lipa-fenix-staging2-1884271.dev.odoo.com",
-            dbName: "lipa-fenix-staging2-1884271",
+           apiUrl: "https://odoo-api-url.com",
+            dbName: "odoo-db-name",
             userName: "admin",
             password: "admin"
         );
