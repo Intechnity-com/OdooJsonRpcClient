c03282c0da2822eeb32adb1297f8814a3bde0b3d
patryk.bujalla <patryk.bujalla@portacapena.com>
2021-04-28 16:19:37 +0200

    add tests to CompanyRepository

--- PortaCapena.OdooJsonRpcClient.Example/CompanyRepository.cs
+++ PortaCapena.OdooJsonRpcClient.Example/CompanyRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using FluentAssertions;
+using PortaCapena.OdooJsonRpcClient.Consts;
 using PortaCapena.OdooJsonRpcClient.Shared;
 using PortaCapena.OdooJsonRpcClient.Shared.Models;
 using Xunit;
@@ -8,7 +9,7 @@ namespace PortaCapena.OdooJsonRpcClient.Example
 {
     public class CompanyOdooRepository : TestBase
     {
-        public class CompanyRepository : OdooRepository<ResCompanyOdooModel>
+        private class CompanyRepository : OdooRepository<ResCompanyOdooModel>
         {
             public CompanyRepository() : base(Config) { }
         }
@@ -19,6 +20,67 @@ namespace PortaCapena.OdooJsonRpcClient.Example
             var repo = new CompanyRepository();
 
             var result = await repo.Query().ToListAsync();
+
+            result.Error.Should().BeNull();
+            result.Succeed.Should().BeTrue();
+            result.Value.Should().NotBeNull().And.NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_By_Id()
+        {
+            var repo = new CompanyRepository();
+
+            var result = await repo.Query().ById(1).FirstOrDefaultAsync();
+
+            result.Error.Should().BeNull();
+            result.Succeed.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Get_By_Ids()
+        {
+            var repo = new CompanyRepository();
+
+            var result = await repo.Query().ByIds(1, 2).ToListAsync();
+
+            result.Error.Should().BeNull();
+            result.Succeed.Should().BeTrue();
+            result.Value.Should().NotBeNull().And.NotBeEmpty();
+            result.Value.Length.Should().Be(2);
+        }
+
+        [Fact]
+        public async Task Get_By_Name()
+        {
+            var repo = new CompanyRepository();
+
+            var result = await repo.Query().Where(x => x.Name, OdooOperator.EqualsTo, "My Company").FirstOrDefaultAsync();
+
+            result.Error.Should().BeNull();
+            result.Succeed.Should().BeTrue();
+            result.Value.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Get_By_Country()
+        {
+            var repo = new CompanyRepository();
+
+            var result = await repo.Query().Where(x => x.CountryCode, OdooOperator.EqualsTo, "BE").ToListAsync();
+
+            result.Error.Should().BeNull();
+            result.Succeed.Should().BeTrue();
+            result.Value.Should().NotBeNull().And.NotBeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_By_Country_name()
+        {
+            var repo = new CompanyRepository();
+
+            var result = await repo.Query().Where<ResCountryOdooModel>(x => x.CountryId, x => x.Name, OdooOperator.EqualsTo, "Belgium").ToListAsync();
 
             result.Error.Should().BeNull();
             result.Succeed.Should().BeTrue();
--- PortaCapena.OdooJsonRpcClient.Shared/TestBase.cs
+++ PortaCapena.OdooJsonRpcClient.Shared/TestBase.cs
@@ -5,8 +5,8 @@ namespace PortaCapena.OdooJsonRpcClient.Shared
     public class TestBase
     {
         protected static readonly OdooConfig Config = new OdooConfig(
-            apiUrl: "https://darling-uat-2324412.dev.odoo.com",
-            dbName: "darling-uat-2324412",
+            apiUrl: "https://db-name.dev.odoo.com",
+            dbName: "db-name",
             userName: "admin",
             password: "admin"
         );
